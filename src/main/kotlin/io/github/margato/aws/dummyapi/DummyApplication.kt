c4be1977add43704d085b6a2f2e2815cc88d0e2a
Osvaldo Margato <15236942+margato@users.noreply.github.com>
2021-03-23 19:46:14 -0300

    rename main class

--- src/main/kotlin/io/github/margato/aws/dummyapi/DummyApplication.kt
+++ src/main/kotlin/io/github/margato/aws/dummyapi/DummyApplication.kt
@@ -4,8 +4,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class TrainingApplication
+class DummyApplication
 
 fun main(args: Array<String>) {
-    runApplication<TrainingApplication>(*args)
+    runApplication<DummyApplication>(*args)
 }
